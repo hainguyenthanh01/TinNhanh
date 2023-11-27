fa7355b8b216ff32705aa773bd3769aa663fb17b
Hunglt <luutienhung29@gmail.com>
2023-11-28 00:50:14 +0700

    update 28/11

--- server_app/API/Controller/address.controller.js
+++ server_app/API/Controller/address.controller.js
@@ -6,9 +6,17 @@ const Wards = require('../../Models/wards')
 module.exports.province = async (req, res) => {
 	try {
 		const province = await Province.find()
-		res.status(200).json({ message: 'Successfully!', data: province, code: 200 })
+		let data = [];
+		province.map(it => {
+			const dataConvert = {
+				label: it.name,
+				value: it.province_id
+			}
+			data.push(dataConvert)
+		})
+		res.status(200).json({ message: 'Successfully!', data: data, code: 200 })
 	} catch (error) {
-		res.status(400).json({ error })
+		res.status(200).json({ message: 'error!', data: [], code: 201 })
 	}
 
 }
@@ -18,9 +26,17 @@ module.exports.district = async (req, res) => {
 		const { province_id } = req.query
 		if (province_id) {
 			const district = await District.find({ province_id })
-			res.status(200).json({ message: 'Successfully!', data: district, code: 200 })
+			let data = [];
+			district.map(it => {
+				const dataConvert = {
+					label: it.name,
+					value: it.district_id
+				}
+				data.push(dataConvert)
+			})
+			res.status(200).json({ message: 'Successfully!', data: data, code: 200 })
 		} else {
-			res.status(200).json({ message: 'Successfully!', data: [], code: 200 })
+			res.status(200).json({ message: 'error!', data: [], code: 201 })
 		}
 
 	} catch (error) {
@@ -34,9 +50,17 @@ module.exports.wards = async (req, res) => {
 		const { district_id } = req.query
 		if (district_id) {
 			const wards = await Wards.find({ district_id })
-			res.status(200).json({ message: 'Successfully!', data: wards, code: 200 })
+			let data = [];
+			wards.map(it => {
+				const dataConvert = {
+					label: it.name,
+					value: it.wards_id
+				}
+				data.push(dataConvert)
+			})
+			res.status(200).json({ message: 'Successfully!', data: data, code: 200 })
 		} else {
-			res.status(200).json({ message: 'Successfully!', data: [], code: 200 })
+			res.status(200).json({ message: 'error!', data: [], code: 201 })
 		}
 
 	} catch (error) {
