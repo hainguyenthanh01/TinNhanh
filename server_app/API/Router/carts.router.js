5fd3c51eca82ed016cf3a6ec95c91d7f976ddad6
Hunglt <luutienhung29@gmail.com>
2023-11-30 13:53:13 +0700

    update 30/11

--- server_app/API/Router/carts.router.js
+++ server_app/API/Router/carts.router.js
@@ -7,6 +7,7 @@ const Cart = require('../Controller/carts.controller')
 router.get('/', Cart.index)
 router.post('/', Cart.postCart)
 router.delete('/', Cart.deleteCart)
+router.delete('/all', Cart.deleteAllCart)
 
 
 module.exports = router
